e644b1bb0708133729f806b39a3b904e05c9d203
divinedavis <divinejdavis@gmail.com>
2015-08-19 12:49:39 -0400

    Calculated the average of 5 numbers

--- Assignment 1/Assignment 1/ViewController.swift
+++ Assignment 1/Assignment 1/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberField: UITextField!
+    @IBOutlet weak var secondNumberField: UITextField!
+    @IBOutlet weak var thirdNumberField: UITextField!
+    @IBOutlet weak var fourthNumberField: UITextField!
+    @IBOutlet weak var fifthNumberField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func calculateAverage(sender: AnyObject) {
+        
+        let firstNumber : Int? = firstNumberField.text.toInt()
+        let secondNumber : Int? = secondNumberField.text.toInt()
+        let thirdNumber : Int? = thirdNumberField.text.toInt()
+        let fourthNumber : Int? = fourthNumberField.text.toInt()
+        let fifthNumber : Int? = fifthNumberField.text.toInt()
+        
+        let sum = firstNumber! + secondNumber! + thirdNumber! + fourthNumber! + fifthNumber!
+        
+        let average = sum/5
+        
+        outputLabel.text = "Average is \(average)"
+        
+        
+
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
